41f86650a71ec995ac1f440ae77b658208d7c210
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-10-07 20:54:04 +0200

    Úkol č.1 a č.2

---
+++ src/components/Registration/registration.jsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react';
+
+const Registration = () => {
+  const [user, setUser] = useState({
+    username: '',
+    email: '',
+    password: '',
+    passwordConfirm: '',
+  });
+};
