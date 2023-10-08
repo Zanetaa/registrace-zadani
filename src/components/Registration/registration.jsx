7d0b43ab701248a40917e504a1e4544a0096a42c
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-10-08 11:15:58 +0200

    Úkoly č.3,4 a 5

--- src/components/Registration/registration.jsx
+++ src/components/Registration/registration.jsx
@@ -8,6 +8,64 @@ const Registration = () => {
     password: '',
     passwordConfirm: '',
   });
+
+  const handleClick = (e) => {
+    e.preventDefault();
+    console.log(user);
+  };
+
+  const handleEmail = (e) => {
+    if ('email' && email.substring(0, email.indexOf('@'))) {
+      setUser({
+        ...user,
+        email: e.target.value,
+        username: e.target.value.split('@')[0],
+      });
+    }
+  };
+
+  return (
+    <div className="Registration">
+      <h1>REGISTRATION</h1>
+      <form onSubmit={handleClick}>
+        <input
+          type="email"
+          placeholder="Email Address"
+          value={user.email}
+          onChange={handleEmail}
+        />
+
+        <input
+          type="text"
+          placeholder="User Name"
+          value={user.username}
+          onChange={(e) => {
+            setUser({ ...user, username: e.target.value });
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={user.password}
+          onChange={(e) => {
+            setUser({ ...user, password: e.target.value });
+          }}
+        />
+
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          value={user.passwordConfirm}
+          onChange={(e) => {
+            setUser({ ...user, passwordConfirm: e.target.value });
+          }}
+        />
+
+        <button type="submit">REGISTER</button>
+      </form>
+    </div>
+  );
 };
 
 export default Registration;
