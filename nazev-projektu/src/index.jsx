cdabd99d4c75ec6c0f991dd63b7dcd76c4cdbc97
Zanetaa <91408269+Zanetaa@users.noreply.github.com>
2023-10-08 12:53:38 +0200

    úpravy

--- nazev-projektu/src/index.jsx
+++ nazev-projektu/src/index.jsx
@@ -1,26 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import './style.css';
+import Registration from './component/Registrationn/reistr';
 
 const App = () => {
   return (
     <div className="container">
-      <div className="container"></div>
-      <header>
-        <div className="logo" />
-        <h1>React webová aplikace Ahoj </h1>
-      </header>
-      <main>
-        <p>
-          Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí{' '}
-          <a href="https://www.npmjs.com/package/create-czechitas-app">
-            create-czechitas-app
-          </a>
-          .
-        </p>
-      </main>
-      <footer>
-        <p>Czechitas, Digitální akademie: Web</p>
-      </footer>
+      <Registration />
     </div>
   );
 };
